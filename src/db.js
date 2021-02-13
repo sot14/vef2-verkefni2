0beba2a56860e1344a45a1ea9d280d3c076354df
sothorsteins <sothorsteins@gmail.com>
2021-02-13 02:06:05 +0000

    Fix db

--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const {
   DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
 } = process.env;
 
 
@@ -15,7 +16,7 @@ if (!connectionString) {
 
 const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
 
-const pool = new pg.Pool({ connectionString });
+const pool = new pg.Pool({ connectionString, ssl });
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
