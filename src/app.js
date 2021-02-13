91ad8291c297d100b2b182b48683bb765643fb4b
sothorsteins <sothorsteins@gmail.com>
2021-02-13 02:27:39 +0000

    css fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { utils } from './utils.js';
 dotenv.config();
 
 const {
-  PORT: port = 3000
+  PORT: port = process.env.port || 3000
 } = process.env;
 
 const app = express();
@@ -64,5 +64,5 @@ app.use(notFoundHandler);
 
 // Verðum að setja bara *port* svo virki á heroku
 app.listen(port, () => {
-  console.info(`Server running at http://localhost:${port}/`);
+  console.info(`Server running at http://host:${port}/`);
 });
